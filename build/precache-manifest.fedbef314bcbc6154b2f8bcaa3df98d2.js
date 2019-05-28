9ffdb105ba81d9ea450c3cca01553f08b84a905a
Joseph Pappano <jepappano4@Josephs-MacBook-Pro.local>
2019-05-28 00:31:18 -0500

    updating

---
+++ build/precache-manifest.fedbef314bcbc6154b2f8bcaa3df98d2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "582d39900feab9b2aa1d",
+    "url": "/katevk1/proj3/static/css/main.e6e368d8.chunk.css"
+  },
+  {
+    "revision": "582d39900feab9b2aa1d",
+    "url": "/katevk1/proj3/static/js/main.945eec43.chunk.js"
+  },
+  {
+    "revision": "6ede8ec3a5d753b010d4",
+    "url": "/katevk1/proj3/static/js/runtime~main.9c980f4f.js"
+  },
+  {
+    "revision": "9c1fb9737739a1e309d4",
+    "url": "/katevk1/proj3/static/js/2.a26f6651.chunk.js"
+  },
+  {
+    "revision": "9f9f725104ba04f55793ba93476192b6",
+    "url": "/katevk1/proj3/index.html"
+  }
+];

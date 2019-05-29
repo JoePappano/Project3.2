4696f863fce618a256a83cd0a7d37fc226446eab
Joseph Pappano <jepappano4@Josephs-MacBook-Pro.local>
2019-05-28 21:17:15 -0500

    updating

--- server.js
+++ server.js
@@ -69,7 +69,7 @@ if(process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'client/build')));
   //
   app.get('*', (req, res) => {
-    res.sendfile(path.join(__dirname, "build", 'insdex.html'));
+    res.sendfile(path.join(__dirname, "build", 'index.html'));
   })
 }
 //build mode

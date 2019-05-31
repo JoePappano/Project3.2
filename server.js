c39f45177c85ec5c9e6426655984fb562f4fff1c
Joseph Pappano <jepappano4@Josephs-MacBook-Pro.local>
2019-05-30 22:27:43 -0500

    updating

--- server.js
+++ server.js
@@ -61,21 +61,23 @@ app.use(flash());
 require("./controller/apiRoutes")(app);
 // require("./config/connection")(app);
 
-//Static file declaration
-app.use('/static', express.static(path.join(__dirname, 'build')));
+app.use(express.static("./client/public/"));
 
-//production mode
-if(process.env.NODE_ENV === 'production') {
-  app.use('/static', express.static(path.join(__dirname, 'build')));
-  //
-  app.get('*', (req, res) => {
-    res.sendfile(path.join(__dirname, "build", 'index.html'));
-  })
-}
-//build mode
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname+'/client/public/index.html'));
-})
+// //Static file declaration
+// app.use('/static', express.static(path.join(__dirname, 'build')));
+
+// //production mode
+// if(process.env.NODE_ENV === 'production') {
+//   app.use('/static', express.static(path.join(__dirname, 'build')));
+//   //
+//   app.get('*', (req, res) => {
+//     res.sendfile(path.join(__dirname, "build", 'index.html'));
+//   })
+// }
+// //build mode
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname+'/client/public/index.html'));
+// })
 
 
 

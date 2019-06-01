de7226b3570238cdd4cdebd7955f63e9c99e8639
Joseph Pappano <jepappano4@Josephs-MacBook-Pro.local>
2019-06-01 18:06:15 -0500

    updating

--- server.js
+++ server.js
@@ -13,24 +13,6 @@ var path = require('path');
 var app = express();
 
 
-// //Static file declaration
-// app.use('/static', express.static(path.join(__dirname, 'build')));
-
-//production mode
-if(process.env.NODE_ENV === 'production') {
-  // app.use('/static', express.static(path.join(__dirname, 'build')));
-  app.use(express.static(path.join(__dirname, "/build")));
-  //
-  app.get('*', (req, res) => {
-    res.sendfile(path.join(__dirname + "/build/index.html"));
-  })
-}
-// //build mode
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname+'/Project3.2/build/index.html'));
-// })
-
-
 
 var PORT = process.env.PORT || 3001;
 
@@ -80,6 +62,23 @@ require("./controller/apiRoutes")(app);
 // require("./config/connection")(app);
 
 // app.use(express.static("./client/build/"));
+
+// //Static file declaration
+// app.use('/static', express.static(path.join(__dirname, 'build')));
+
+//production mode
+if(process.env.NODE_ENV === 'production') {
+  // app.use('/static', express.static(path.join(__dirname, 'build')));
+  app.use(express.static('client/build'));
+  //
+  app.get('*', (req, res) => {
+    res.sendfile(path.resolve(__dirname, "client", "build", "index.html"));
+  })
+}
+// //build mode
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname+'/Project3.2/build/index.html'));
+// })
 
 
 var syncOptions = { force: false };
